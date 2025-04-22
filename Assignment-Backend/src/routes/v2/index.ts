c9f9ea82d6f3fc872d443d79bd6c624a60eda31f
Deepesh <51400881+DeepeshC612@users.noreply.github.com>
2025-04-23 00:10:11 +0530

    Delete Assignment-Backend directory

--- Assignment-Backend/src/routes/v2/index.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { basePath } from "../../utils/routingPath";
-
-const routerV2 = express.Router();
-
-routerV2.use(basePath.User.base, (req, res) => {res.send("Not implemented yet")});
-
-export default routerV2;
